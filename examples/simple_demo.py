849ea14804a7f851966ddc1f86e7e636299f2808
Kaustubh Chaudhuri <kaustubhc2014@gmail.com>
2025-08-28 20:48:16 +0530

    Updated demo file

--- examples/simple_demo.py
+++ examples/simple_demo.py
@@ -1,7 +1,12 @@
 from feature_store import opendata
+from feature_store import store
 
 # import sys
 # import os
 # sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-print(opendata.fetch_city_roads('Thane, India').shape)
+sample_area = opendata.fetch_city_roads('Thane, India')
+print(sample_area.shape)
+
+datastore = store.GeoFeatureStore()
+datastore.save(sample_area, 'Thane_India')
