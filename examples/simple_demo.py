99b2d71461fe95835b452935566fb2bbc06ddae8
Kaustubh Chaudhuri <kaustubhc2014@gmail.com>
2025-09-02 22:15:51 +0530

    Added load example

--- examples/simple_demo.py
+++ examples/simple_demo.py
@@ -5,8 +5,16 @@ from feature_store import store
 # import os
 # sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-sample_area = opendata.fetch_city_roads('Thane, India')
+sample_area = opendata.fetch_city_roads("Thane, India")
 print(sample_area.shape)
 
+# Instantiate datastore object
 datastore = store.GeoFeatureStore()
-datastore.save(sample_area, 'Thane_India')
+
+# Save the new locations
+datastore.save(sample_area, "Thane_India")
+
+# Load location dataxx
+gdf = datastore.load("Thane_India")
+print(gdf.head())
+
